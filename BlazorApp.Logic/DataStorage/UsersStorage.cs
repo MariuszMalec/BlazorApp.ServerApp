d4eedd6b2b493cbef02f5373ad1f69e33096e6b7
Mariusz Malec <mario106c@gmail.com>
2023-01-31 19:37:21 +0100

    add user details

--- BlazorApp.Logic/DataStorage/UsersStorage.cs
+++ BlazorApp.Logic/DataStorage/UsersStorage.cs
@@ -6,10 +6,10 @@ namespace BlazorApp.Logic.DataStorage
     {
         public static List<User> Users = new List<User>
         {
-            new User {FirstName = "Mariusz", LastName = "Malec", Address="Sadowa", AddressCorrespondence="",Email="mm@example.com", PhoneMobile="501622578", PhoneNumber=null},
-            new User {FirstName = "Sebastian", LastName = "Malec" , Address="Dabkowo", AddressCorrespondence="",Email="sm@example.com", PhoneMobile=null, PhoneNumber="552391376"},
-            new User {FirstName = "Basia", LastName = "Malec" , Address="Sadkowo", AddressCorrespondence="",Email="bm@example.com", PhoneMobile=null, PhoneNumber="552391376"},
-            new User {FirstName = "Maniek", LastName = "Staniek" , Address="Stankowo", AddressCorrespondence="",Email="ms@example.com", PhoneMobile=null, PhoneNumber="552391376"}
+            new User {Id=1, FirstName = "Mariusz", LastName = "Malec", Address="Sadowa", AddressCorrespondence="",Email="mm@example.com", PhoneMobile="501622578", PhoneNumber=null},
+            new User {Id=2, FirstName = "Sebastian", LastName = "Malec" , Address="Dabkowo", AddressCorrespondence="",Email="sm@example.com", PhoneMobile=null, PhoneNumber="552391376"},
+            new User {Id=3, FirstName = "Basia", LastName = "Malec" , Address="Sadkowo", AddressCorrespondence="",Email="bm@example.com", PhoneMobile=null, PhoneNumber="552391376"},
+            new User {Id=4, FirstName = "Maniek", LastName = "Staniek" , Address="Stankowo", AddressCorrespondence="",Email="ms@example.com", PhoneMobile=null, PhoneNumber="552391376"}
         };
     }
 }
